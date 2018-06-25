2c886a8b4f0a6410ff7ffaf405c2244a22ff5b00
rajyalakshmivadlamannati <rajirajyalakshmia6@gmail.com>
2018-06-25 03:32:51 -0700

    first commit

--- main.js
+++ main.js
@@ -12,9 +12,23 @@ function loadJson(file,callback){
 loadJson("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
+  fun_career(data.careerobjective);
   fun_education(data.education);
+  fun_achieve(data.achievements);
+  fun_skill(data.technicalskills);
   });
   var div2=document.getElementById("child2");
+  function fun_career(careerobj){
+    var headLine=document.createElement("h2");
+    headLine.textContent="Career Objective";
+    div2.appendChild(headLine);
+    var underLine=document.createElement("hr");
+    div2.appendChild(underLine);
+    var info=document.createElement("p");
+    info.textContent=careerobj.info;
+    info.style.fontSize="17px";
+    div2.appendChild(info);
+  }
   function fun_education(edu){
     var headLine=document.createElement("h2");
     headLine.textContent="Educational Qualifications";
@@ -35,5 +49,37 @@ loadJson("data.json",function(text){
         list.appendChild(listItem1);
         list.appendChild(listItem2);
         list.appendChild(listItem3);
+
+      }
+    }
+    function fun_achieve(art){
+      var headLine=document.createElement("h2");
+      headLine.textContent="Achievements";
+      div2.appendChild(headLine);
+      var underLine=document.createElement("hr");
+      div2.appendChild(underLine);
+      var list=document.createElement("ul");
+      div2.appendChild(list);
+     for(var j=0;j<art.length;j++){
+         var listItem=document.createElement("li");
+         listItem.innerHTML=art[j].nameofachivement;
+          list.appendChild(listItem);
     }
   }
+    function fun_skill(techs)
+    {
+      var headLine=document.createElement("h2");
+      headLine.textContent="Technical Skills";
+      div2.appendChild(headLine);
+      var underLine=document.createElement("hr");
+      div2.appendChild(underLine);
+      var tr="";
+      var table=document.createElement("table");
+      table.border="1";
+      div2.appendChild(table);
+      for(var k=0;k<techs.length;k++)
+      {
+        tr+="<tr><td>"+techs[k].name+"</td><td>"+techs[k].data+"</td></tr>";
+      }
+     table.innerHTML=tr;
+    }
